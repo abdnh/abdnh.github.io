cd0536369a2f955e99a0a6b9e1757985f6ea0ec3
abdnh <abdnh@users.noreply.github.com>
2022-03-21 04:33:04 +0000

    jekyll build from Action 2af5c9ab058c6512ebcd3d65bcf7ca50f180f9d4

---
+++ deploy.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Based on https://github.com/yegor256/jekyll-github-deploy/blob/master/bash/deploy.sh
+
+set -x
+set -e
+set -o pipefail
+
+URL=$(git config --get remote.origin.url)
+BRANCH="gh-pages"
+BRANCH_FROM="master"
+BUNDLE=""
+DRAFTS=$6
+SRC=$(pwd)
+TEMP=$(mktemp -d -t jgd-XXX)
+trap "rm -rf ${TEMP}" EXIT
+CLONE=${TEMP}/clone
+COPY=${TEMP}/copy
+
+echo ${CLONE}
+
+echo -e "Cloning Github repository:"
+git clone -b "${BRANCH_FROM}" "${URL}" "${CLONE}"
+cp -R ${CLONE} ${COPY}
+
+cd "${CLONE}"
+
+echo -e "\nBuilding Jekyll site:"
+rm -rf _site
+
+
+jekyll build
+
+if [ ! -e _site ]; then
+  echo -e "\nJekyll didn't generate anything in _site!"
+  exit -1
+fi
+
+cp -R _site ${TEMP}
+
+cd ${TEMP}
+rm -rf ${CLONE}
+mv ${COPY} ${CLONE}
+cd ${CLONE}
+
+echo -e "\nPreparing ${BRANCH} branch:"
+if [ -z "$(git branch -a | grep origin/${BRANCH})" ]; then
+  git checkout --orphan "${BRANCH}"
+else
+  git checkout "${BRANCH}"
+fi
+
+echo -e "\nDeploying into ${BRANCH} branch:"
+rm -rf *
+cp -R ${TEMP}/_site/* .
+rm -f README.md
+git add .
+git commit -am "new version $(date)" --allow-empty
+git push origin ${BRANCH} 2>&1 | sed 's|'$URL'|[skipped]|g'
+
+echo -e "\nCleaning up:"
+rm -rf "${CLONE}"
+rm -rf "${SITE}"
